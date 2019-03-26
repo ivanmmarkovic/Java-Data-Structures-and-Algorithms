bdc38a5bdbed07b35fcd74a29d8f1924bb920aba
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-03-26 12:12:44 -0700

    Update Vertex.java

--- 07.Graphs/DepthFirstSearch/Vertex.java
+++ 07.Graphs/DepthFirstSearch/Vertex.java
@@ -1,19 +1,14 @@
+package recap;
 
 public class Vertex {
 	
 	String label;
 	boolean visited;
 	int index;
-	
 	public Vertex(String label) {
 		this.label = label;
 		this.visited = false;
-	}
-	
-	public Vertex(String label, int index) {
-		this.label = label;
-		this.visited = false;
-		this.index = index;
+		this.index = 0;
 	}
 
 }
