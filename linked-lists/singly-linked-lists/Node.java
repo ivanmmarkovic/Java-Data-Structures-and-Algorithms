2c0f598aaccb3714bb7445200f4efd9a0271fe4b
Ivan Markovic <ivanmdmarkovic@gmail.com>
2019-07-22 06:49:39 -0700

    Update Node.java

--- linked-lists/singly-linked-lists/Node.java
+++ linked-lists/singly-linked-lists/Node.java
@@ -1,25 +1,17 @@
-
 public class Node {
 	
-	public int info;
-	public Node next;
-	public Node() {
-		this.info = 0;
+	int payload;
+	Node next;
+	public Node(int payload) {
+		this.payload = payload;
 		this.next = null;
 	}
-	public Node(int i) {
-		this.info = i;
-		this.next = null;
-	}
-	public Node(int i, Node n) {
-		info = i;
-		next = n;
-	}
-	public int print() {
-		return info;
+	public Node(int payload, Node next) {
+		this.payload = payload;
+		this.next = next;
 	}
 	public boolean isEqual(int i) {
-		return info == i;
+		return payload == i;
 	}
 
 }
