13115f81ca2f6eab31305d093d3c537dea9f10be
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2019-09-11 19:41:38 -0700

    Circular Singly Linked List

---
+++ linkedLists/circularSinglyLinkedList/Node.java
@@ -0,0 +1,17 @@
+package linkedLists.circularSinglyLinkedList;
+
+public class Node {
+
+    Node next;
+    int payload;
+
+    public Node(int payload){
+        this.payload = payload;
+        this.next = null;
+    }
+
+    public Node(int payload, Node next){
+        this.payload = payload;
+        this.next = next;
+    }
+}
