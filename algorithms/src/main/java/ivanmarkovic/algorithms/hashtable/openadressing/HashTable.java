0c9541c294f197965d2a7372ff2bd37a8915e509
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2019-11-05 22:29:41 +0100

    Add Hash Table

---
+++ algorithms/src/main/java/ivanmarkovic/algorithms/hashtable/openadressing/HashTable.java
@@ -0,0 +1,14 @@
+package ivanmarkovic.algorithms.hashtable.openadressing;
+
+public interface HashTable<K extends Number, V> {
+
+	void put(K key, V value);
+
+    V get(K key);
+
+    void delete(K key);
+
+    int length();
+
+    boolean contains(K key);
+}
