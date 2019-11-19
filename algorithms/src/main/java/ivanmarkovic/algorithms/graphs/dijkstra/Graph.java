c1a9b7a38bbfa6bc307d8f6170c40171ddf4811c
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2019-11-19 14:07:47 -0800

    Finished Graph and Vertex

--- algorithms/src/main/java/ivanmarkovic/algorithms/graphs/dijkstra/Graph.java
+++ algorithms/src/main/java/ivanmarkovic/algorithms/graphs/dijkstra/Graph.java
@@ -20,7 +20,7 @@ public class Graph {
 		this.adjacencyList = new HashMap<String, List<Vertex>>();
 	}
 	
-	public void addEdge(String label) {
+	public void addVertex(String label) {
 		if(this.pointer == this.size)
 			return;
 		Vertex vertex = new Vertex(label, Integer.MAX_VALUE, this.pointer);
@@ -29,6 +29,26 @@ public class Graph {
 		this.adjacencyList.put(label, new ArrayList<Vertex>());
 	}
 	
+	public void addEdge(String label1, String label2, int weight) {
+		int index = this.getIndexByLabel(label2);
+		Vertex vertex = new Vertex(label2, weight, index);
+		this.adjacencyList.get(label1).add(vertex);
+	}
+	
+	public Integer getIndexByLabel(String label) {
+		boolean found = false;
+		int count = 0;
+		while(count < this.pointer && !found) {
+			if(this.vertices[count].getLabel().equals(label))
+				found = true;
+			else
+				count++;
+		}
+		if(found)
+			return count;
+		else
+			return null;
+	}
 	
 
 }
--- algorithms/src/main/java/ivanmarkovic/algorithms/graphs/dijkstra/Vertex.java
+++ algorithms/src/main/java/ivanmarkovic/algorithms/graphs/dijkstra/Vertex.java
@@ -17,4 +17,37 @@ public class Vertex {
 		this.index = index;
 	}
 
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+	
+	
 }
