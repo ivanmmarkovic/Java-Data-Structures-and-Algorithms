cac099a39538659a8b723166539a20e36b1c5ebe
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2019-11-19 16:44:12 -0800

    Cycle detection - directed graph

--- algorithms/src/main/java/ivanmarkovic/algorithms/graphs/cycledetection/directedgraph/Graph.java
+++ algorithms/src/main/java/ivanmarkovic/algorithms/graphs/cycledetection/directedgraph/Graph.java
@@ -45,20 +45,17 @@ public class Graph {
 		this.adjacencyList.get(label1).add(label2);
 	}
 	
-	public void dfs(String start, String prev) {
-		this.previous.put(start, prev);
+	public void dfs(String start) {
 		this.colors.put(start, "gray");
 		this.entry.put(start, ++this.time);
-		if(prev != null) {
-			this.distance.put(start, this.distance.get(prev) + 1);
-		}
 		for(String neighbour: this.adjacencyList.get(start)) {
 			if(this.colors.get(neighbour).equals("gray")) {
 				System.out.println("CYCLE DETECTED");
 				return;
 			}
 			if(this.colors.get(neighbour).equals("white")) {
-				this.dfs(neighbour, start);
+				this.previous.put(neighbour, start);
+				this.dfs(neighbour);
 			}
 		}
 		this.colors.put(start, "black");
