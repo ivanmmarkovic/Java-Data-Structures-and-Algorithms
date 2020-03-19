b6e81acfb03df93e61d54dca76ccf2ef0a004cfc
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2020-03-19 14:39:34 -0700

    BinaryTree

---
+++ algorithms/src/main/java/ivanmarkovic/algorithms/trees/binarytree/TreeNode.java
@@ -0,0 +1,63 @@
+package ivanmarkovic.algorithms.trees.binarytree;
+
+public class TreeNode {
+	
+	TreeNode parent, leftChild, rightChild;
+	int key;
+	
+	public TreeNode() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public TreeNode(int key) {
+		this.key = key;
+	}
+	
+	public TreeNode(int key, TreeNode parent){
+		this.key = key;
+		this.parent = parent;
+	}
+	
+	public boolean hasLeftChild() {
+		return this.leftChild != null;
+	}
+	
+	public boolean hasRightChild() {
+		return this.rightChild != null;
+	}
+	
+	public boolean isLeaf() {
+		return !this.hasLeftChild() && !this.hasRightChild();
+	}
+	
+	public boolean hasBothChildren() {
+		return this.hasLeftChild() && this.hasRightChild();
+	}
+	
+	public boolean hasParent() {
+		return this.parent != null;
+	}
+	
+	public boolean isLeftChild() {
+		return this.parent.leftChild == this;
+	}
+	
+	public boolean isRightChild() {
+		return this.parent.rightChild == this;
+	}
+	
+	public TreeNode findMin() {
+		if(this.hasLeftChild())
+			return this.leftChild.findMin();
+		else
+			return this;
+	}
+	
+	public TreeNode findMax() {
+		if(this.hasRightChild())
+			return this.rightChild.findMax();
+		else
+			return this;
+	}
+}
+
