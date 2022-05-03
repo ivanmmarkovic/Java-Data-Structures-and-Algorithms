db5f8cecf9056991f8ae760de1cde14c4be9bfc8
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-05-03 14:28:00 +0100

    algorithms/src/main/java/ivanmarkovic/algorithms/recursion/FibonacciRecursiveSolution.java

--- algorithms/src/main/java/ivanmarkovic/algorithms/recursion/FibonacciRecursiveSolution.java
+++ algorithms/src/main/java/ivanmarkovic/algorithms/recursion/FibonacciRecursiveSolution.java
@@ -8,16 +8,16 @@ public class FibonacciRecursiveSolution {
 	}
 	
 	public static int fibonacci(int n) {
-		return fibonacciHelper(n, 0, 0, 1);
+		return fibonacciHelper(n, 0, 1);
 	}
 	
-	private static int fibonacciHelper(int n, int acc, int prev, int curr) {
+	private static int fibonacciHelper(int n, int prev, int curr) {
 		if(n == 1)
 			return prev;
 		else if(n == 2)
 			return curr;
 		else 
-			return fibonacciHelper(n - 1, acc + curr, curr, prev + curr);
+			return fibonacciHelper(n - 1, curr, prev + curr);
 	}
 
 }
