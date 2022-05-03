070f1760d60abf6ded995e56e07cb73541fa4e51
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-05-03 14:01:25 +0100

    algorithms/src/main/java/ivanmarkovic/algorithms/recursion/FibonacciWorstSolution.java

---
+++ algorithms/src/main/java/ivanmarkovic/algorithms/recursion/FibonacciWorstSolution.java
@@ -0,0 +1,20 @@
+package ivanmarkovic.algorithms.recursion;
+
+public class FibonacciWorstSolution {
+	
+	
+	public static void main(String args[]) {
+		for(int i = 1; i <= 6; i++)
+			System.out.println("Fibonacci " + i + " is " + fibonacci(i));
+	}
+	
+	public static int fibonacci(int n) {
+		if(n == 1)
+			return 0;
+		else if (n == 2) 
+			return 1;
+		else
+			return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
+}
