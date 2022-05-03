7326eeee42c9f4889a6ac0887219bb7e37676929
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-05-03 13:56:43 +0100

    factorialIterative

--- algorithms/src/main/java/ivanmarkovic/algorithms/recursion/Factorial.java
+++ algorithms/src/main/java/ivanmarkovic/algorithms/recursion/Factorial.java
@@ -6,6 +6,11 @@ public class Factorial {
 		
 		for(int i = 0; i <= 6; i++)
 			System.out.println("Factorial of " + i + " is " + factorialRecursive(i));
+		
+		System.out.println("-------------------------------------------------------");
+		
+		for(int i = 0; i <= 6; i++)
+			System.out.println("Factorial of " + i + " is " + factorialIterative(i));
 	}
 	
 	public static int factorialRecursive(int n) {
@@ -14,6 +19,17 @@ public class Factorial {
 		else 
 			return n * factorialRecursive(n - 1);
 
+	}
+	
+	public static int factorialIterative(int n) {
+		if(n <= 1)
+			return 1;
+		int f = 1;
+		while(n > 1) {
+			f *= n;
+			n--;
+		}
+		return f;
 	}
 
 }
